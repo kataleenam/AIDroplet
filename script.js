c8843ec833b71f8abb72bdb62c56d9631a533023
Kataleena Mishra <kitkatmishra.km@gmail.com>
2025-05-01 21:52:17 -0400

    working warning popup

--- script.js
+++ script.js
@@ -10,10 +10,23 @@ let level = getComputedStyle(document.documentElement).getPropertyValue('--level
  */
 const change = 25;
 
+/**
+ * boolean tracking if popup is hidden or not
+ */
+let hidden = true;
+
 // functions
 
-function alertUser() {
-
+/**
+ * controls visibily of popup
+ */
+function warning() {
+    if (hidden) {
+        document.getElementById('warning').style.display = "block";
+    } else {
+        document.getElementById('warning').style.display = "none";
+    }
+    hidden = !hidden;
 }
 
 /**
@@ -23,19 +36,20 @@ function addWater() {
     // converts level from a string to an int
     level = parseInt(level);
 
+    // checks if bottle is full, then warns the user and resets level
+    if (level == 0){
+        level = 200;
+        warning();
+    } 
+
     level = level - change;
 
      // converts level back to a string and sets new value
      level = String(level);
      level += 'px';
      document.documentElement.style.setProperty('--level', level);
-
-
-    // checks if bottle is full, then warns the user and resets level
-    if (level == '0px'){
-        level = '200px';
-    } 
 }
 
-// functioncalls
+// function calls
 document.getElementById('addBtn').addEventListener("click", addWater);
+document.getElementById('warning-btn').addEventListener("click", warning);
